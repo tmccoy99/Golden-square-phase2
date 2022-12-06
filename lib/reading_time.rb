9f15c18227c20a5c3983bb0c85b20d0be553703a
Thomas McCoy <finnmccoy99@gmail.com>
2022-12-06 15:50:54 +0000

    reading time now works with decimals and passes all tests

--- lib/reading_time.rb
+++ lib/reading_time.rb
@@ -2,7 +2,8 @@ load 'count_words.rb'
 
 def reading_time(text)
   count = count_words(text)
-  time_in_mins = count / 200
-  return  "This text will take #{time_in_mins} minutes"  if time_in_mins > 1 
+  time_in_mins = count / 200.to_f
+  time_in_mins = time_in_mins.to_i if time_in_mins.to_s[-2..-1] == ".0"
+  return  "This text will take #{time_in_mins} minutes" unless time_in_mins == 1 
   "This text will take #{time_in_mins} minute"
 end
