e1f8c23e4448d288e111ded5ffc956acf9f1154d
Tom Rolfe <tomrolfe20@gmail.com>
2022-12-06 15:32:01 +0000

    added two more tests and 2/3 work

--- lib/reading_time.rb
+++ lib/reading_time.rb
@@ -1,3 +1,8 @@
+load 'count_words.rb'
+
 def reading_time(text)
-  
+  count = count_words(text)
+  time_in_mins = count / 200
+  return  "This text will take #{time_in_mins} minutes"  if time_in_mins > 1 
+  "This text will take #{time_in_mins} minute"
 end
--- spec/reading_time_spec.rb
+++ spec/reading_time_spec.rb
@@ -6,4 +6,14 @@ RSpec.describe "testing reading_time" do
       expect(reading_time("hi " * 200)).to eq "This text will take 1 minute"
     end
   end
+  context "given a different multiple of 200 word text" do
+    it "returns time in minutes" do
+      expect(reading_time("hi " * 400)).to eq "This text will take 2 minutes"
+    end
+  end
+  context "given a non multiple of 200" do
+    it "return in decimal minutes" do
+      expect(reading_time("hi " * 300)).to eq "This text will take 1.5 minutes" 
+    end
+  end
 end
