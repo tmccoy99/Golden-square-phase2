fbbe669210d0bf4be76ca2615fd7a101bd2875d7
Tom Rolfe <tomrolfe20@gmail.com>
2022-12-06 16:30:45 +0000

    Excercise 2 done

--- lib/grammar_checker.rb
+++ lib/grammar_checker.rb
@@ -1,2 +1,7 @@
 def grammar_checker(text)
+    first_letter = text[0]
+    last_letter = text[-1]
+    fail("Doesn't have a capital at the start") unless first_letter.upcase == first_letter
+    fail("Doesn't have punctuation at the end.") unless ['?','!','.'].include?(last_letter)
+    'This has passed'
 end
--- spec/grammar_checker_spec.rb
+++ spec/grammar_checker_spec.rb
@@ -1,2 +1,19 @@
 require "grammar_checker"
 
+RSpec.describe "Testing grammar checker" do
+    context 'When starts with lowercase letter' do
+        it 'fails' do
+            expect{ grammar_checker('hello') }.to raise_error "Doesn't have a capital at the start"
+        end 
+    end
+    context "When doesn't finish with appropriate punctuation" do
+        it 'fails' do
+            expect{ grammar_checker('Hello') }.to raise_error "Doesn't have punctuation at the end."
+        end
+    end
+    context 'Starts with capital and ends with punctuation' do
+        it 'Passes' do
+            expect(grammar_checker('Hello!')).to eq 'This has passed' 
+        end
+    end
+end
