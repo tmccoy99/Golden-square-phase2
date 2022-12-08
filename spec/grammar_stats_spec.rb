1aedf5e43a12a1bf800f82726a92520700ff6e00
Finn McCoy <finnmccoy99@gmail.com>
2022-12-08 13:32:50 +0000

    Wrote tests for the percentage_good method

--- spec/grammar_stats_spec.rb
+++ spec/grammar_stats_spec.rb
@@ -17,4 +17,24 @@ RSpec.describe GrammarStats do
       expect(gs.check("hello.")).to eq false
     end
   end
+  
+  context "When percentage_good is called before any check calls" do
+    it "returns 100" do
+      gs = GrammarStats.new
+      expect(gs.percentage_good).to eq 100
+    end
+  end
+  
+  context "When percentage_good is called after check calls" do
+    it "returns rounded integer percentage of check calls that returned true" do
+      gs = GrammarStats.new
+      gs.check("Hello")
+      expect(gs.percentage_good).to eq 0
+      gs.check("Banana.")
+      expect(gs.percentage_good).to eq 50
+      gs.check("You're a wizard Harry.")
+      expect(gs.percentage_good).to eq 67
+    end
+  end
 end
+
