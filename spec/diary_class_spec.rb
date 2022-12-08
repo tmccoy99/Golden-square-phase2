660161c19fbb074d17dfad6892f591d07cd0f86c
Finn McCoy <finnmccoy99@gmail.com>
2022-12-08 11:27:43 +0000

    Refactored test instances of DiaryEntry and added new reading_chunk test to determine whether contents will be repeated after finishing

--- spec/diary_class_spec.rb
+++ spec/diary_class_spec.rb
@@ -1,9 +1,9 @@
 require "diary_class"
 
 RSpec.describe DiaryEntry do
-  entry = DiaryEntry.new("Tuesday", "Today was great")
   context "When provided title and contents" do
     it "Can return them" do
+      entry = DiaryEntry.new("Tuesday", "Today was great")
       expect(entry.title).to eq "Tuesday"
       expect(entry.contents).to eq "Today was great"
     end
@@ -11,40 +11,47 @@ RSpec.describe DiaryEntry do
   
   context "When count_words called" do
     it "returns number of words in contents as int" do
+      entry = DiaryEntry.new("Tuesday", "Today was great")
       expect(entry.count_words).to eq 3
     end
   end
   
   context "When has contents and reading_time is called" do
     it "Returns the estimated reading time of contents" do
-      entry2 = DiaryEntry.new("HI", "hi " * 1000)
-      expect(entry2.reading_time(200)).to eq 5
+      entry = DiaryEntry.new("HI", "hi " * 1000)
+      expect(entry.reading_time(200)).to eq 5
     end
   end
   
   context "reading_chunk is called with wpm*reading_time >= contents.length" do
     it "returns context" do
-      entry2 = DiaryEntry.new("HI", "hi " * 1000)
-      expect(entry2.reading_chunk(1000, 1)).to eq entry2.contents.chop
+      entry = DiaryEntry.new("HI", "hi " * 1000)
+      expect(entry.reading_chunk(1000, 1)).to eq entry2.contents.chop
     end
   end
   
   context "reading_chunk is called with wpm*reading_time < contents.length" do
     it "returns first wpm*reading_time words of contents" do
-      entry2 = DiaryEntry.new("HI", "hi " * 1000)
-      expect(entry2.reading_chunk(500, 1)).to eq ("hi " * 500).chop
+      entry = DiaryEntry.new("HI", "hi " * 1000)
+      expect(entry.reading_chunk(500, 1)).to eq ("hi " * 500).chop
     end
   end
   
   context "reading_chunk called multiple times" do
     it "sequentially returns chunks of contents" do
       testing_contents = ("Hello " * 5) + ("there " * 5) + ("chap " * 3)
-      entry3 = DiaryEntry.new("Greeting", testing_contents)
-      expect(entry3.reading_chunk(5, 1)).to eq ("Hello " * 5).chop
-      expect(entry3.reading_chunk(5, 1)).to eq ("there " * 5).chop
-      expect(entry3.reading_chunk(5, 1)).to eq ("chap " * 3).chop
+      entry = DiaryEntry.new("Greeting", testing_contents)
+      expect(entry.reading_chunk(5, 1)).to eq ("Hello " * 5).chop
+      expect(entry.reading_chunk(5, 1)).to eq ("there " * 5).chop
+      expect(entry.reading_chunk(5, 1)).to eq ("chap " * 3).chop
     end
   end
   
-  
+  context "reading_chunk called again after finishing contents" do
+    it "will start returning contents from beginning do" do
+      entry = DiaryEntry.new("News", "The country isn't doing well")
+      entry.reading_chunk(5, 1)
+      expect(entry.reading_chunk(2, 2)).to eq "The country isn't doing"
+    end
+  end
 end
