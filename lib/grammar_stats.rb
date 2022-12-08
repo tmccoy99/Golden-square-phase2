b2f0f857353418ddcde18563c5daed3976c5a36e
Finn McCoy <finnmccoy99@gmail.com>
2022-12-08 13:06:46 +0000

    Created grammar stats class

---
+++ lib/grammar_stats.rb
@@ -0,0 +1,14 @@
+class GrammarStats
+  def initialize
+    # ...
+  end
+
+  def check(text)
+    true
+  end
+
+  def percentage_good
+    # Returns as an integer the percentage of texts checked so far that passed
+    # the check defined in the `check` method. The number 55 represents 55%.
+  end
+end
