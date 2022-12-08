ccc4df021ee208384da88d88ed64ce999a1a1797
Finn McCoy <finnmccoy99@gmail.com>
2022-12-08 10:11:38 +0000

    DiaryEntry class now passes first three tests

---
+++ lib/diary_class.rb
@@ -0,0 +1,33 @@
+class DiaryEntry
+  def initialize(title, contents) # title, contents are strings
+    @title = title ; @contents = contents
+  end
+
+  def title
+    @title
+  end
+
+  def contents
+    @contents
+  end
+
+  def count_words
+    @contents.split.length
+  end
+
+  def reading_time(wpm) # wpm is an integer representing the number of words the
+    @contents.split.length/(wpm.to_f).round
+    
+  end
+
+  def reading_chunk(wpm, minutes) # `wpm` is an integer representing the number
+                                  # of words the user can read per minute
+                                  # `minutes` is an integer representing the
+                                  # number of minutes the user has to read
+    # Returns a string with a chunk of the contents that the user could read
+    # in the given number of minutes.
+    # If called again, `reading_chunk` should return the next chunk, skipping
+    # what has already been read, until the contents is fully read.
+    # The next call after that it should restart from the beginning.
+  end
+end
