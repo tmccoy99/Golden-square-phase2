7408900ec4547a4f242becd2f91236fa980bfe60
Finn McCoy <finnmccoy99@gmail.com>
2022-12-08 11:59:37 +0000

    Added a contents_words instance variable on initaliasation to avoid repeated calls of contents.split and thus reduce runtime

--- lib/diary_class.rb
+++ lib/diary_class.rb
@@ -1,6 +1,7 @@
 class DiaryEntry
   def initialize(title, contents) # title, contents are strings
     @title = title ; @contents = contents
+    @contents_words = @contents.split
   end
 
   def title
@@ -12,11 +13,11 @@ class DiaryEntry
   end
 
   def count_words
-    @contents.split.length
+    @contents_words.length
   end
 
   def reading_time(wpm) # wpm is an integer representing the number of words the
-    @contents.split.length/(wpm.to_f).round
+    self.count_words/(wpm.to_f).round
     
   end
 
@@ -24,7 +25,7 @@ class DiaryEntry
     @saved_idx ||= 0
     max_words = wpm * minutes
     end_idx = @saved_idx + max_words
-    chunk = @contents.split[@saved_idx...end_idx].join(" ")
+    chunk = @contents_words[@saved_idx...end_idx].join(" ")
     @saved_idx = end_idx >= self.count_words ? 0 : end_idx
     chunk
   end
