8b85c88982ea11e38f074d5ff457d95643ebe69e
Finn McCoy <finnmccoy99@gmail.com>
2022-12-08 13:15:18 +0000

    Added tests for cases where check method should return false

--- spec/grammar_stats_spec.rb
+++ spec/grammar_stats_spec.rb
@@ -8,4 +8,13 @@ RSpec.describe GrammarStats do
       expect(gs.check("Hello.")).to eq true
     end
   end
+  
+  context "When check is called on a non grammatical string" do
+    it "returns false" do
+      gs = GrammarStats.new
+      expect(gs.check("hello")).to eq false
+      expect(gs.check("Hello")).to eq false
+      expect(gs.check("hello.")).to eq false
+    end
+  end
 end
