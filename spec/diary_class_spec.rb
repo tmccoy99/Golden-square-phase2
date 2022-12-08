e978a57b8ff2bf7c1625bbfa3a83235ae7a673e2
Finn McCoy <finnmccoy99@gmail.com>
2022-12-08 10:10:47 +0000

    Wrote three tests for DiaryEntry class methods

---
+++ spec/diary_class_spec.rb
@@ -0,0 +1,25 @@
+require "diary_class"
+
+RSpec.describe DiaryEntry do
+  entry = DiaryEntry.new("Tuesday", "Today was great")
+  context "When provided title and contents" do
+    it "Can return them" do
+      expect(entry.title).to eq "Tuesday"
+      expect(entry.contents).to eq "Today was great"
+    end
+  end
+  
+  context "When count_words called" do
+    it "returns number of words in contents as int" do
+      expect(entry.count_words).to eq 3
+    end
+  end
+  
+  entry2 = DiaryEntry.new("HI", "hi " * 1000)
+  context "When has contents and reading_time is called" do
+    it "Returns the estimated reading time of contents" do
+      expect(entry2.reading_time(200)).to eq 5
+    end
+  end
+  
+end
