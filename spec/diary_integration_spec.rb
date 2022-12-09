f32ac05c37740a9b9aa0daf84671500c596dfea5
Finn McCoy <finnmccoy99@gmail.com>
2022-12-09 13:57:41 +0000

    Added tests for find_best_entry_for_reading_time

--- spec/diary_integration_spec.rb
+++ spec/diary_integration_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe "Diary integration testing" do
   context "When contains saved DiaryEntrys" do
     entry1 = DiaryEntry.new("title", "contents " * 200)
     entry2 = DiaryEntry.new("title", "more contents " * 200)
+    entry3 = DiaryEntry.new("title", "even more contents " * 200)
     
     context "When all is called" do
       it "returns the entries in an array" do
@@ -33,6 +34,18 @@ RSpec.describe "Diary integration testing" do
         diary.add(entry2)
         expect(diary.reading_time(200)).to eq 3
         expect(diary.reading_time(300)).to eq 2
+      end
+    end
+    context "When find_best_entry_for_reading_time is called" do
+      it "returns longest entry that can be read in available time" do
+        diary = Diary.new
+        diary.add(entry1)
+        expect(diary.find_best_entry_for_reading_time(199, 1)).to eq entry1
+        expect(diary.find_best_entry_for_reading_time(200, 1)).to eq entry1
+        diary.add(entry2)
+        expect(diary.find_best_entry_for_reading_time(400, 1)).to eq entry2
+        diary.add(entry3)
+         expect(diary.find_best_entry_for_reading_time(600, 1)).to eq entry3
       end
     end
         
