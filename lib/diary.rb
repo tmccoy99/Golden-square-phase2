52e4fa4a07c51eb5344857bf0983cdc4bea5ab7b
Finn McCoy <finnmccoy99@gmail.com>
2022-12-09 10:55:29 +0000

    Diary class passes first elementary tests

---
+++ lib/diary.rb
@@ -0,0 +1,34 @@
+load "diary_entry.rb"
+
+class Diary
+  def initialize
+  end
+
+  def add(entry) # entry is an instance of DiaryEntry
+    # Returns nothing
+  end
+
+  def all
+    []
+  end
+
+  def count_words
+    0
+  end
+
+  def reading_time(wpm) # wpm is an integer representing
+                        # the number of words the user can read per minute
+    # Returns an integer representing an estimate of the reading time in minutes
+    # if the user were to read all entries in the diary.
+  end
+
+  def find_best_entry_for_reading_time(wpm, minutes)
+        # `wpm` is an integer representing the number of words the user can read
+        # per minute.
+        # `minutes` is an integer representing the number of minutes the user
+        # has to read.
+    # Returns an instance of diary entry representing the entry that is closest 
+    # to, but not over, the length that the user could read in the minutes they
+    # have available given their reading speed.
+  end
+end
