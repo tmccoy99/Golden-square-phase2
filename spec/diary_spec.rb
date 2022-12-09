130da1e010c283d4aea0937c339129cd9e8020a5
Finn McCoy <finnmccoy99@gmail.com>
2022-12-09 11:13:56 +0000

    Each viable method now has a test for the case of an instance of Diary with no saved DiaryEntrys

--- spec/diary_spec.rb
+++ spec/diary_spec.rb
@@ -11,5 +11,11 @@ RSpec.describe Diary do
     it "returns 0 when count_words called" do
       expect(diary.count_words).to eq 0
     end
+    it "returns 0 when reading_time called" do
+      expect(diary.reading_time(200)).to eq 0
+    end
+    it "raises error when find_best_entry_for_reading_time called" do
+      expect{ diary.find_best_entry_for_reading_time(0, 0) }.to raise_error "Error, no entries in diary"
+    end 
   end
 end
