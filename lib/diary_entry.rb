e0871a5bfb3f0e9f4a2f3503ff10aa9bc6fa7c58
Finn McCoy <finnmccoy99@gmail.com>
2022-12-09 12:16:33 +0000

    Discovered and fixed bug where reading_time method was not rounding answer properly

--- lib/diary_entry.rb
+++ lib/diary_entry.rb
@@ -17,8 +17,7 @@ class DiaryEntry
   end
 
   def reading_time(wpm) # wpm is an integer representing the number of words the
-    self.count_words/(wpm.to_f).round
-    
+   (self.count_words/(wpm.to_f)).round
   end
 
   def reading_chunk(wpm, minutes)
@@ -30,13 +29,3 @@ class DiaryEntry
     chunk
   end
 end
-
-# `wpm` is an integer representing the number
-                                  # of words the user can read per minute
-                                  # `minutes` is an integer representing the
-                                  # number of minutes the user has to read
-    # Returns a string with a chunk of the contents that the user could read
-    # in the given number of minutes.
-    # If called again, `reading_chunk` should return the next chunk, skipping
-    # what has already been read, until the contents is fully read.
-    # The next call after that it should restart from the beginning.
